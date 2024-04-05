6f5c62e4f17558c41cf3253d885e9bc365997447
ALUNO SESI CAMARAGIBE <aluno.sesicamaragibe@SISTEMAFIEPE.LOCAL>
2024-04-05 09:03:26 -0300

    criação

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 print('hello word')
+a = 5
+b = 9
+if a > b :
+    print("este numero e o maior")
+else:
+    print("este numero não e o maior")
+
+if b > a : 
+    print("este numero e o maior")
+else:
+    print("este numero não e o maior")
