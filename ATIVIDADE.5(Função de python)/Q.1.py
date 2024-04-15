a62f21db311cf53fa419363338d826048957d94a
DaniloPS23 <165788344+DaniloPS23@users.noreply.github.com>
2024-04-15 16:21:05 -0300

    Update Q.1.py

--- ATIVIDADE.5(Função de python)/Q.1.py
+++ ATIVIDADE.5(Função de python)/Q.1.py
@@ -1,8 +1,10 @@
 1 - Faça um programa para imprimir: 1 2 2 3 3 3 ..... n n n n n n ... n
 para um informado pelo usuário. Use uma função que receba um valor inteiro e imprima até a n-ésima linha.
 
-def imprimir_padrao(n):
+def imprimir_padrao2(n):
     for i in range(1, n + 1):
-        print((str(i) + ' ') * i)
+        for j in range(i):
+            print(i, end=" ")
+        print()
 n = int(input("Digite o valor de n: "))
-imprimir_padrao(n)
+imprimir_padrao2(n)
