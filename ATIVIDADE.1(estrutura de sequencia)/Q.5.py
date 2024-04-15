02438e628cefd564a4af24b0bcfadb24ed42cf34
DaniloPS23 <165788344+DaniloPS23@users.noreply.github.com>
2024-04-15 14:34:14 -0300

    Create Q.5.py

---
+++ ATIVIDADE.1(estrutura de sequencia)/Q.5.py
@@ -0,0 +1,5 @@
+5 - Faça um Programa que converta metros para centímetros.
+
+metros = float(input("digite a quantidaded metros: "))
+centimetros = metros*100
+print(f"{metros:.0f} metros e igual a {centimetros:.0f} centimetros")
