9237e5a2f4b45ab589d4f9776a5d0166162244b4
DaniloPS23 <165788344+DaniloPS23@users.noreply.github.com>
2024-04-15 14:50:40 -0300

    Create Q.1.py

---
+++ ATIVIDADE.2(Estrutura de comparação)/Q.1.py
@@ -0,0 +1,9 @@
+1 - Faça um Programa que peça um valor e mostre na tela se o valor é positivo ou negativo.
+
+valor=float(input('digite um numero: '))
+if valor > 0:
+ print('O valor e positivo')
+elif valor<0:
+  print('O valor e negativo')
+else:
+  print('o valor e zero')
