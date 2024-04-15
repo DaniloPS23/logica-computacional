0ce94f1fdf7748efaec8dcf3177f8f57a4789581
DaniloPS23 <165788344+DaniloPS23@users.noreply.github.com>
2024-04-15 14:37:28 -0300

    Delete ATIVIDADE.1(estrutura de sequencia)/Q.10.py

--- ATIVIDADE.1(estrutura de sequencia)/Q.10.py
+++
@@ -1,5 +0,0 @@
-10 - Faça um Programa que peça a temperatura em graus Celsius, transforme e mostre em graus Fahrenheit.
-
-celcius=float(input('mostre a temperatura em celcius: '))
-fahrenheit=(celcius*9/5)+32
-print(f'a temperautura em celcius é {fahrenheit:.1f} °F')
